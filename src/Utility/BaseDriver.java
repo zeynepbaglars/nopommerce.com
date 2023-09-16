275061c82645f0db81447049f6cf2dd2c18431db
ZeynepBaglars <zeynepbaglars@gmail.com>
2023-09-16 04:01:06 +0300

    loc ve elements kısmı

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -1,16 +1,11 @@
 package Utility;
 
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-
 import java.time.Duration;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -32,38 +27,6 @@ public class BaseDriver {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
-    }
-
-    public void login() {
-
-        driver.get("https://openmrs.org");
-
-        WebElement demoButton = driver.findElement(By.xpath("(//*[@href='https://openmrs.org/demo/'])[2]"));
-        demoButton.click();
-
-
-        WebElement mrs2Button = driver.findElement(By.xpath("(//span[@class='elementor-button-text'])[2]"));
-        mrs2Button.click();
-
-        MyFunc.bekle(2);
-        WebElement mrs2DemoButton = driver.findElement(By.xpath("(//span[@class='elementor-button-text'])[4]"));
-        mrs2DemoButton.click();
-
-        wait.until(ExpectedConditions.urlToBe("https://demo.openmrs.org/openmrs/login.htm"));
-        WebElement name = driver.findElement(By.id("username"));
-        name.sendKeys("Admin");
-
-        WebElement password = driver.findElement(By.id("password"));
-        password.sendKeys("Admin123");
-
-        MyFunc.bekle(2);
-        WebElement location = driver.findElement(By.id("Inpatient Ward"));
-        location.click();
-
-        WebElement login = driver.findElement(By.id("loginButton"));
-        login.click();
-
-        Assert.assertTrue(driver.getCurrentUrl().equals("https://demo.openmrs.org/openmrs/referenceapplication/home.page"), "Yanlık kullanıcı adı veya şifre");
     }
 
 
