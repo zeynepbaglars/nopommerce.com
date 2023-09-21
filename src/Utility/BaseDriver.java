d25c60101d8d15ade3c6bac843ae478a6c59c81d
mustafayumrutepe <mstfyumrutepe@gmail.com>
2023-09-22 01:01:49 +0300

    new branch acildi

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -1,9 +1,12 @@
 package Utility;
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import java.time.Duration;
